ecc9345dfcdefdf92dc1001e07bd8c1ec20f99a2
Joathan Souza <joathanf@gmail.com>
2024-11-18 20:19:36 -0300

    adds cron configuration for ScrapeTaskWorker on Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'sidekiq-cron'
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
@@ -7,3 +9,9 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
+
+Sidekiq::Cron::Job.create(
+  name: 'ScrapeTaskWorker - Runs every 1 minute',
+  cron: '*/1 * * * *',
+  class: 'ScrapeTaskWorker'
+)
