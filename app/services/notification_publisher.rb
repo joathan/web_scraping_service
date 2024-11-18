dd68da5597ec653b90c2a69be24a1befafaf0186
Joathan Souza <joathanf@gmail.com>
2024-11-18 19:20:03 -0300

    refactors NotificationPublisher to manage connections and queues more robustly

--- app/services/notification_publisher.rb
+++ app/services/notification_publisher.rb
@@ -3,30 +3,59 @@
 require 'bunny'
 
 class NotificationPublisher
+  QUEUE_NAME = 'notifications.scrapes.completed'
+
   def initialize
-    @connection = Bunny.new(
-      host: ENV['RABBITMQ_HOST'] || 'localhost',
-      port: ENV['RABBITMQ_PORT'] || 5672,
-      user: ENV['RABBITMQ_DEFAULT_USER'] || 'guest',
-      password: ENV['RABBITMQ_DEFAULT_PASS'] || 'guest'
+    @connection = nil
+    @channel = nil
+    @queue = nil
+  end
+
+  def publish(message)
+    validate_message!(message)
+    queue.publish(message.to_json, persistent: true)
+  rescue StandardError => e
+    handle_error(e)
+  end
+
+  private
+
+  def connection
+    @connection ||= Bunny.new(
+      host: ENV.fetch('RABBITMQ_HOST', 'localhost'),
+      port: ENV.fetch('RABBITMQ_PORT', 5672).to_i,
+      user: ENV.fetch('RABBITMQ_DEFAULT_USER', 'guest'),
+      password: ENV.fetch('RABBITMQ_DEFAULT_PASS', 'guest')
     ).start
-    @channel = @connection.create_channel
-    @queue = @channel.queue(
-      'notifications.scrapes.completed',
+  end
+
+  def channel
+    @channel ||= connection.create_channel
+  end
+
+  def queue
+    @queue ||= channel.queue(
+      QUEUE_NAME,
       durable: true,
       exclusive: false,
       auto_delete: false
     )
-    ensure_queue_exists
   end
 
-  def publish(message)
-    @queue.publish(message.to_json, persistent: true)
+  def validate_message!(message)
+    raise ArgumentError, 'Message must be a Hash' unless message.is_a?(Hash)
   end
 
-  def ensure_queue_exists
-    @queue.status
-  rescue Bunny::NotFound
-    raise 'Queue does not exist!'
+  def handle_error(exception)
+    # Log the error or send to an error tracking service
+    Rails.logger.error("Failed to publish message: #{exception.message}")
+    raise exception
+  end
+
+  def close_connection
+    @channel&.close
+    @connection&.close
+  rescue StandardError => e
+    Rails.logger.warn("Failed to close RabbitMQ connection: #{e.message}")
   end
 end
