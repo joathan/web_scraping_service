6714f96be8ed58f5a1c08e230b9daeda51a0ed38
Joathan Souza <joathanf@gmail.com>
2024-11-18 20:08:48 -0300

    adds NotificationService to manage task notifications

---
+++ app/services/notification_service.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class NotificationService
+  def initialize(task)
+    @task = task
+  end
+
+  def notify
+    publisher = NotificationPublisher.new
+    publisher.publish(notification_payload)
+    Rails.logger.info({ message: "Notification Sent", task_id: @task.id }.to_json)
+  rescue StandardError => e
+    Rails.logger.error({ message: "Notification Error", task_id: @task.id, error: e.message }.to_json)
+    raise e
+  end
+
+  private
+
+  def notification_payload
+    {
+      scrape_task_id: @task.id,
+      url: @task.url,
+      results: @task.results
+    }
+  end
+end
