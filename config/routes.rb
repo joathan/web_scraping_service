4c9a9d3fc18270561a256e030cd777c61e29a0f7
Joathan Souza <joathanf@gmail.com>
2024-11-18 18:46:27 -0300

    adds basic authentication and route to Sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV['SIDEKIQ_USERNAME'] && password == ENV['SIDEKIQ_PASSWORD']
+  end
+
+  mount Sidekiq::Web => '/sidekiq'
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  namespace :api do
+    namespace :v1 do
+      resources :scrapes, only: %i[create show]
+    end
+  end
 end
