f0543f695c4c2661b7a20ee97a31bc9e872dd295
Joathan Souza <joathanf@gmail.com>
2024-11-19 11:39:46 -0300

    adds ExecutorService to perform scraping tasks with error handling

---
+++ app/services/scraping/executor_service.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Scraping
+  class ExecutorService
+    def initialize(task)
+      @task = task
+    end
+
+    def perform
+      response = HTTParty.get(@task.url)
+      return { status: :failed } unless response.code == 200
+
+      document = Nokogiri::HTML(response.body)
+      { status: :success, data: parse_data(document) }
+    rescue StandardError => e
+      Rails.logger.error({ message: "Scraping Error", task_id: @task.id, error: e.message }.to_json)
+      { status: :failed }
+    end
+
+    private
+
+    def parse_data(document)
+      items = document.css('ul.listahorizontal li').map do |item|
+        {
+          label: item.at_css('h6')&.text&.strip,
+          value: item.at_css('span.destaque')&.text&.strip
+        }
+      end.reject { |item| item[:label].nil? && item[:value].nil? }
+
+      items << price(document)
+    end
+
+    def price(document)
+      {
+        label: 'Preço',
+        value: document.at_css('.preco')&.text&.strip
+      }
+    end
+  end
+end
