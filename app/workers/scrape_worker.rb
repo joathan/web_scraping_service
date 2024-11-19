a75ddfb767171c4a9cd0817e0d4ed9282f4408c2
Joathan Souza <joathanf@gmail.com>
2024-11-19 11:43:08 -0300

    changes ProcessorService to Scraping::ProcessorService in ScrapeWorker

--- app/workers/scrape_worker.rb
+++ app/workers/scrape_worker.rb
@@ -6,7 +6,7 @@ class ScrapeWorker
   def perform(task_id)
     task = ScrapeTask.find(task_id)
 
-    Task::ProcessorService.new(task).process
+    Scraping::ProcessorService.new(task).process
   rescue StandardError => e
     Rails.logger.error({ message: "Worker Error", task_id: task_id, error: e.message }.to_json)
 
