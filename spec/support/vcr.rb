15f06b7b35df830e5e5cdea2d9a1c7548e0d188e
Joathan Souza <joathanf@gmail.com>
2024-11-18 21:49:44 -0300

    refactor VCR configuration to change cassette directory and add request filtering for example.com

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,8 +4,13 @@ require 'vcr'
 require 'webmock/rspec'
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.ignore_localhost = true
+  config.allow_http_connections_when_no_cassette = false
   config.configure_rspec_metadata!
+
+  config.ignore_request do |request|
+    URI(request.uri).host == 'example.com'
+  end
 end
