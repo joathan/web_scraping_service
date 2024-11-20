ff88d8dcf2f21bacf253c95ef21401c90c952aaa
Joathan Souza <joathanf@gmail.com>
2024-11-20 20:53:22 -0300

    adds token support in ScrapeWorker's perform method

--- app/workers/scrape_worker.rb
+++ app/workers/scrape_worker.rb
@@ -3,10 +3,10 @@
 class ScrapeWorker
   include Sidekiq::Worker
 
-  def perform(task_id)
+  def perform(task_id, token)
     task = ScrapeTask.find(task_id)
 
-    Scraping::ProcessorService.new(task).process
+    Scraping::ProcessorService.new(task, token).process
   rescue StandardError => e
     Rails.logger.error({ message: "Worker Error", task_id: task_id, error: e.message }.to_json)
 
