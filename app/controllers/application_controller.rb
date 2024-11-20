4525efa63d20d3d7a7bb21e55c172773c70238f4
Joathan Souza <joathanf@gmail.com>
2024-11-20 11:21:26 -0300

    add request authentication in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  before_action :authorize_request
+
+  attr_reader :current_user
+
+  private
+
+  def authorize_request
+    token = request.headers['Authorization']&.split(' ')&.last
+    auth_response = AuthenticationService.verify_token(token)
+
+    if auth_response
+      @current_user ||= auth_response
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
