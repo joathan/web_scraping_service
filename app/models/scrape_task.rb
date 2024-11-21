e87685a094d22b366e6a15d427eb646e4b637d55
Joathan Souza <joathanf@gmail.com>
2024-11-21 12:39:53 -0300

    adds new 'in_progress' status to ScrapeTask model

--- app/models/scrape_task.rb
+++ app/models/scrape_task.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ScrapeTask < ApplicationRecord
-  enum status: { pending: 0, completed: 1, failed: 2 }
+  enum status: { pending: 0, completed: 1, in_progress: 2, failed: 3 }, _default: "pending"
 
   validates :url, presence: true, url: true
   validates :status, presence: true
